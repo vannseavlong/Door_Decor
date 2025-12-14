9a975e6c3ef0601b5280af3b19237a8b1aecb770
vannseavlong <137812405+LONGCELOT@users.noreply.github.com>
2025-12-14 17:38:29 +0700

    prevent multiple create api call

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 const LANGS = ["en", "kh"];
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const parts = pathname.split("/");
   const first = parts[1];
